c6e939e1e37c6375c04ef280548aac4a9ca55219
dansmith09 <dansmith_09@hotmail.com>
2023-05-12 17:19:51 +1000

    scrape validator rewards and calculates NO and LST yeilds

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-async function run() {
+async function main() {
 
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
@@ -13,16 +13,32 @@ async function run() {
     // const title = await page.evaluate(() => document.title);
     // console.log(title) // logs page title
     const text = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('#monetary-premium .leading-normal'), (e) => ({
-        text: e.innerHTML,
-    })))
+    Array.from(document.querySelectorAll('#monetary-premium .leading-normal'), (e) => e.innerHTML))
 
-    console.log(text.length)    
-    console.log(text[40].text)
+    console.log(text[40].split('').splice(0,3).join('')) // Prints: 6.6 // ETH staking APY
+    const apy = parseFloat(text[40].split('').splice(0,3).join('')) // Prints: 6.6 // ETH staking APY // number
 
-    // '//*[@id="monetary-premium"]/div[2]/div[1]/div[2]/div[2]/a[4]/span[3]'
+    console.log(`
+    ---------------------------------------
+    Base Validator Rewards
+    ---------------------------------------
+    Solo Staking: ${apy}%
+    ---------------------------------------
+    Node Opperator Rewards
+    ---------------------------------------    
+    Rocket Pool Minipool (16 ETH): ${(apy * 1.15).toFixed(2)}%
+    Rocket Pool LEB8 (8 ETH): ${(apy * 1.42).toFixed(2)}%
+    Stader NO: ${(apy * 1.22).toFixed(2)}%
+    ---------------------------------------
+    Liquid Staking Tokens Rewards
+    ---------------------------------------    
+    Stader ETHx LST ${(apy * 0.9).toFixed(2)}%
+    stETH LST ${(apy * 0.9).toFixed(2)}%
+    rETH LST: : ${(apy * 0.86).toFixed(2)}%
+    shgETH Unit Trust: : ~${(((apy*1.42)*(1/(11/8)))*0.85).toFixed(2)}%
+    `)
 
     await browser.close();
 }
 
-run();
+main();
